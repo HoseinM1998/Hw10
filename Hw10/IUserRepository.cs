97f8602cb4cdf996fbd0fccf4e874a6e2baec603
HosseinM1998 <hmoslehi98@gmail.com>
2024-10-30 22:04:55 +0330

    First Project

---
+++ Hw10/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿
+    public interface IUserRepository
+    {
+    public void Add(User user);
+    public List<User> GetAll();
+    public User Get(string username);
+    public void Update(string userName, string password, string status);
+    public void Delete(string userName);
+
+    }
+
---
+++ Hw10/User.cs
@@ -0,0 +1,10 @@
+﻿
+public class User
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Status { get; set; } = "available";
+
+}
+
